70979f64c51fb8b47864a02094b2a0bacca06446
Princessbolade <mobolade.akinlua@outlook.com>
2021-02-26 17:23:16 +0100

    Header and Sider Component completed

--- src/components/sider/avatar.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const Avatar = () => <div>Avatar</div>;
-
-export default Avatar;
--- src/components/sider/sider.jsx
+++ src/components/sider/sider.jsx
@@ -1,10 +1,6 @@
-import { Layout, Menu } from "antd";
+import { Layout, Menu, Avatar, Image } from "antd";
 import React from "react";
-import {
-  UploadOutlined,
-  UserOutlined,
-  VideoCameraOutlined,
-} from "@ant-design/icons";
+import AvatarImage from "../../assets/avatar.svg";
 
 const Sider = () => (
   <Layout.Sider
@@ -17,19 +13,39 @@ const Sider = () => (
       console.log(collapsed, type);
     }}
   >
-    <div className="logo" />
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <Avatar size={50} src={<Image src={AvatarImage} />} />
+      <p style={{ color: "white", paddingTop: "5px" }}>Mobolade</p>
+    </div>
+
+    <div className="logo" style={{ paddingBottom: "20px" }} />
     <Menu theme="dark" mode="inline" defaultSelectedKeys={["4"]}>
-      <Menu.Item key="1" icon={<UserOutlined />}>
-        nav 1
+      <Menu.Item key="1">
+        <i style={{ marginRight: "15px" }} className="fas fa-headphones"></i>
+        Discover
       </Menu.Item>
-      <Menu.Item key="2" icon={<VideoCameraOutlined />}>
-        nav 2
+      <Menu.Item key="2">
+        <i style={{ marginRight: "15px" }} className="fas fa-search"></i>
+        Search
       </Menu.Item>
-      <Menu.Item key="3" icon={<UploadOutlined />}>
-        nav 3
+      <Menu.Item key="3">
+        <i style={{ marginRight: "15px" }} className="fas fa-heart"></i>
+        Favorites
       </Menu.Item>
-      <Menu.Item key="4" icon={<UserOutlined />}>
-        nav 4
+      <Menu.Item key="4">
+        <i style={{ marginRight: "15px" }} className="fas fa-play-circle"></i>
+        Playlist
+      </Menu.Item>
+      <Menu.Item key="5">
+        <i style={{ marginRight: "15px" }} className="fas fa-align-left"></i>
+        Charts
       </Menu.Item>
     </Menu>
   </Layout.Sider>
